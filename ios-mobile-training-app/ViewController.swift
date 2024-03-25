ae26a6bc4739d394752f7e2adb14252915720b51
Eduardo Zatoni <eduardo.zatoni@emarsys.com>
2024-03-25 13:20:47 +0100

    Initial Commit

---
+++ ios-mobile-training-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-mobile-training-app
+//
+//  Created by Zatoni Curupana, Eduardo on 25.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
